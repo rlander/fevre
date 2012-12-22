3c58b80bee89886ebb8f239ca3eee9f8ac24e225
Rodrigo Landerdahl <rlanderdahl@gmail.com>
2012-12-22 16:36:02 -0200

    "Name" is a core function.

--- src/fevre/example.clj
+++ src/fevre/example.clj
@@ -10,15 +10,15 @@
 (defn hello
   ([req]
     (response "Hello World!"))
-  ([req name]
-    {:body (str "Hello " name "!")
+  ([req user]
+    {:body (str "Hello " user "!")
      :headers {}
      :status 200}))
 
 (defn sum [req f s]
   (response (str (+ (Integer. f) (Integer. s)))))
 
-(def my-routes (app "/hello/{name}/" #'hello
+(def my-routes (app "/hello/{user}/" #'hello
                     "/hello/"        #'hello
                     ; only matches digit characters [0-9]
                     "/sum/{f:[0-9]+}/{s:[0-9]+}/" "fevre.example/sum"))
